218d57ed9784388412d9ac43b7656d9edc34f5ce
Vedansh Saini <vedanshsaini06@gmail.com>
2025-10-31 17:14:50 +0530

    setup router and controller

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 //todo require("dotenv").config({path: './env'});
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
